523eafc6b4515d170d5f0bd73b371fb4c12c2580
Haugen <tobiashaugen@gmail.com>
2013-03-27 20:18:52 +0100

    Only react on proper keystrokes

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
+	var avalibleKeys = new Array(
+		113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 97, 115, 100, 102, 103, 104,
+		106, 107, 108, 122, 120, 99, 118, 98, 110, 109, 113, 32
+	);
+
+	console.log(avalibleKeys);
+
 	$(window).keypress(function(e) {
-		keystroke(e.which);
+		if ($.inArray(e.which, avalibleKeys) != -1) {
+			keystroke(e.which);
+		}
 	});
 
 	function keystroke(key) {
